3034755f93f90151c36def7d977a7e82ba3f5ff7
Gideon Bature <infoaboutgideon@gmail.com>
2023-08-08 23:33:03 +0100

    remove colon from the continue

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,7 @@
 for i in range(0, 10):
     for j in range(0, 10):
         if i == j or i > j:
-            continue;
+            continue
         if i == 8 and j == 9:
             print("{}{}".format(i, j))
         else:
