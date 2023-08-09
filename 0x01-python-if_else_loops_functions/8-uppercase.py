ac2bbba7a2b2434c1a9e82ea3425ee085be65b5d
Gideon Bature <infoaboutgideon@gmail.com>
2023-08-09 05:48:25 +0100

    remove the last print

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -6,7 +6,6 @@ def uppercase(str):
         if num_char >= 97 and num_char <= 123:
             num_char -= 32
         elif num_char >= 65 and num_char <= 90 or num_char == 32:
-            num_char = num_char
+            pass
         char = chr(num_char)
         print("{}".format(char), end="")
-    print("{}".format(""))
