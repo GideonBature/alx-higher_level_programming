baf008387f4ffbf90913371a2c27209669d0cf96
Gideon Bature <infoaboutgideon@gmail.com>
2023-08-11 23:15:03 +0100

    change range from 7 to 6

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    add = __import__("magic_calculation_102", fromlist=["add"]).add
-    sub = __import__("magic_calculation_102", fromlist=["sub"])
+    add = __import__("magic_calculation_102").add
+    sub = __import__("magic_calculation_102").sub
 
     if a < b:
         c = add(a, b)
-        for i in range(4, 7):
+        for i in range(4, 6):
             c = add(c, i)
         return c
     else:
