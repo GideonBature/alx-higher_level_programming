1f6d52d6a352200bf5baa63f322524b02ed639a1
Gideon Bature <infoaboutgideon@gmail.com>
2023-08-11 18:39:30 +0100

    implements zero directly

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,8 +4,8 @@ if __name__ == "__main__":
     zero = 0
     argv_count = len(sys.argv) - 1
     argv_count_range = len(sys.argv)
-    if len(sys.argv) == 1:
-        print("{:d} arguments.".format(argv_count))
+    if len(sys.argv) <= 1:
+        print("{:d} arguments.".format(zero))
     elif len(sys.argv) > 1:
         print("{:d} arguments:".format(argv_count))
         for i in range(1, argv_count_range):
