f53c892644b423c6a54e1013ec05ca75fc4277e6
Gideon Bature <infoaboutgideon@gmail.com>
2023-08-11 23:12:51 +0100

    add somethings

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
     add = __import__("magic_calculation_102", fromlist=["add"]).add
-    sub = __import__("magic_calculation_102", fromlist=["sub"]).sub
+    sub = __import__("magic_calculation_102", fromlist=["sub"])
 
     if a < b:
         c = add(a, b)
