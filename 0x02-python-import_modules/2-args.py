aa568b3bdae7b97563d25b85a6544a3af4cff795
Gideon Bature <infoaboutgideon@gmail.com>
2023-08-11 18:33:51 +0100

    add if check for __name__ to __main__

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-import sys
-zero = 0
-argv_count = len(sys.argv) - 1
-argv_count_range = len(sys.argv)
-if len(sys.argv) == 1:
-    print("{:d} arguments.".format(zero))
-elif len(sys.argv) > 1:
-    print("{:d} arguments:".format(argv_count))
-    for i in range(1, argv_count_range):
-        print("{:d}: {}".format(i, sys.argv[i]))
+if __name__ == "__main__":
+    import sys
+    zero = 0
+    argv_count = len(sys.argv) - 1
+    argv_count_range = len(sys.argv)
+    if len(sys.argv) == 1:
+        print("{:d} arguments.".format(argv_count))
+    elif len(sys.argv) > 1:
+        print("{:d} arguments:".format(argv_count))
+        for i in range(1, argv_count_range):
+            print("{:d}: {}".format(i, sys.argv[i]))
