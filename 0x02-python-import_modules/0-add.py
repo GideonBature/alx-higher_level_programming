2e35f1530d4bc4dbb4c700d6ea3bb0c1751606d9
Gideon Bature <infoaboutgideon@gmail.com>
2023-08-11 17:49:41 +0100

    correct the main and name

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 from add_0 import add
-__name__ == "__main__"
-a = 1
-b = 2
-res = add(a, b)
-print("{:d} + {:d} = {:d}".format(a, b, res))
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    res = add(a, b)
+    print("{:d} + {:d} = {:d}".format(a, b, res))
