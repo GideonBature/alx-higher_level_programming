08f13a350d32272b8e7133c0a19d3d822ac54d6f
Gideon Bature <infoaboutgideon@gmail.com>
2023-08-15 00:24:48 +0100

    just used normal equal to sign

--- 7-add_tuple.py
+++ 7-add_tuple.py
@@ -13,5 +13,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
         tuple_a = 0, 0
     if len(tuple_b) == 0:
         tuple_b = 0, 0
-    result += (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    result = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
     return result
