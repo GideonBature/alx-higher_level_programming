11b9695240200f8d7dce318c4e75921374a58d03
Gideon Bature <infoaboutgideon@gmail.com>
2023-08-14 19:11:33 +0100

    used the reverse in the loop

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    my_list.reverse()
-    for num in my_list:
+    for num in reversed(my_list):
         print("{:d}".format(num))
