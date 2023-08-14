4b3b3cd5fe31163a02bb6a86da03b7b76f54fd96
Gideon Bature <infoaboutgideon@gmail.com>
2023-08-14 18:35:01 +0100

    use control statement

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -4,8 +4,9 @@ def element_at(my_list, idx):
     len_list = len(my_list)
     if idx < 0:
         return None
-    if idx > len_list:
+    elif idx > len_list - 1:
         return None
-    for num in my_list:
-        if idx == num:
-            return my_list[idx]
+    else:
+        for num in my_list:
+            if idx == num:
+                return my_list[idx]
