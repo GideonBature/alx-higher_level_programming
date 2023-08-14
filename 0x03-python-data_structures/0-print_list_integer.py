e8cd73ddd80ea5fde42a3c1ed0a8a8801e5b49ee
Gideon Bature <infoaboutgideon@gmail.com>
2023-08-14 17:59:04 +0100

    removed index and used the str.format totally

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -3,5 +3,5 @@ def print_list_integer(my_list=[]):
     """prints all integers of a list"""
     for num in my_list:
         f_str = "{}"
-        result = f_str.format(my_list[num - 1])
+        result = f_str.format(num)
         print(result)
