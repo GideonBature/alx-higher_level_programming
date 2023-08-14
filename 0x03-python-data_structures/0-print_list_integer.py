599d9bd5d7e79371c8c5d20d80345968620e66cb
Gideon Bature <infoaboutgideon@gmail.com>
2023-08-14 17:54:51 +0100

    change to loop through the list directly

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     """prints all integers of a list"""
-    len_num = len(my_list)
-    for i in range(0, len_num):
-        print("{}".format(my_list[i]))
+    for num in my_list:
+        f_str = "{}"
+        print(f_str.format(my_list[i]))
