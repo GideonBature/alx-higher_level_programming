91db802347c2176e2aa6b9ede6cf12b25db37afd
Gideon Bature <infoaboutgideon@gmail.com>
2023-08-14 18:04:03 +0100

    used just the str

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,6 +2,4 @@
 def print_list_integer(my_list=[]):
     """prints all integers of a list"""
     for num in my_list:
-        f_str = "{}"
-        result = f_str.format(num)
-        print(result)
+        print(str(num))
