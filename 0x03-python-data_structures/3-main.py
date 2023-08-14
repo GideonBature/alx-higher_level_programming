8ea9fcb4092cf6656994200eefa73b453bda6fc5
Gideon Bature <infoaboutgideon@gmail.com>
2023-08-14 19:21:25 +0100

    a function that replaces an element in a list at a specific position without modifying the original list (like in C).

--- 0x03-python-data_structures/3-main.py
+++ 0x03-python-data_structures/3-main.py
@@ -3,3 +3,10 @@ print_reversed_list_integer = __import__('3-print_reversed_list_integer').print_
 
 my_list = [1, 2, 3, 4, 5]
 print_reversed_list_integer(my_list)
+my_list1 = []
+print_reversed_list_integer(my_list1)
+my_list2 = [-2]
+print_reversed_list_integer(my_list2)
+my_list3 = [3]
+print_reversed_list_integer(my_list3)
+
