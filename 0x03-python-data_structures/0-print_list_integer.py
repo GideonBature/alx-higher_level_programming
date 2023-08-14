c695f33d34dcd9de2a5dc1512cb202f756e224fa
Gideon Bature <infoaboutgideon@gmail.com>
2023-08-14 18:28:07 +0100

    a function that retrieves an element from a list like in C.

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 def print_list_integer(my_list=[]):
     """prints all integers of a list"""
     for num in my_list:
---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    """ retrieves an element from a list like C """
+    len_list = len(my_list)
+    if idx < 0:
+        return None
+    if len_list > len_list:
+        return None
+    for num in my_list:
+        if num == idx:
+            return my_list[idx]
