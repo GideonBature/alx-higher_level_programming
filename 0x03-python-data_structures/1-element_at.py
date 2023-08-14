fb11cd7af1b5a251f06388af79347e59ee5ffa77
Gideon Bature <infoaboutgideon@gmail.com>
2023-08-14 18:32:15 +0100

    correct idx

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -4,8 +4,8 @@ def element_at(my_list, idx):
     len_list = len(my_list)
     if idx < 0:
         return None
-    if idx > len_list:
+    if idx > len_list - 1:
         return None
     for num in my_list:
-        if num == idx:
+        if idx == num:
             return my_list[idx]
