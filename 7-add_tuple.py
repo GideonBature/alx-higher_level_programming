422b63e2184a2fffca6c48a390f9a091ddd168f6
Gideon Bature <infoaboutgideon@gmail.com>
2023-08-15 00:37:24 +0100

    use return statement directly

--- 7-add_tuple.py
+++ 7-add_tuple.py
@@ -13,7 +13,6 @@ def add_tuple(tuple_a=(), tuple_b=()):
     Return:
         A tuple with 2 integers
     """
-    result = tuple()
     if len(tuple_a) < 2:
         if len(tuple_a) == 1:
             tuple_a = tuple_a[0], 0
@@ -25,5 +24,4 @@ def add_tuple(tuple_a=(), tuple_b=()):
         else:
             tuple_b = 0, 0
 
-    result = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-    return result
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
