204ff0f6847f307904347b5d21d731a1d59c83bc
Gideon Bature <infoaboutgideon@gmail.com>
2023-08-15 01:09:07 +0100

    add condition for empty list

--- 0x03-python-data_structures/9-main.py
+++ 0x03-python-data_structures/9-main.py
@@ -10,3 +10,7 @@ print("Max: {}".format(max_value))
 my_list = [-1, -90, -2, -13, -34, -5, -13, -3]
 max_value = max_integer(my_list)
 print("Max: {}".format(max_value))
+my_list = []
+max_value = max_integer(my_list)
+print("Max: {}".format(max_value))
+
