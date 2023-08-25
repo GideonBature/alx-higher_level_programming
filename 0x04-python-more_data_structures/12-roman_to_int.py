a9090c1f839e9e8c972c703a71532d29419c74a9
Gideon Bature <infoaboutgideon@gmail.com>
2023-08-25 09:30:37 +0100

    correct edgecases for None and char

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,9 @@
 
 def roman_to_int(roman_string):
 
-    if not isinstance(roman_string, str) or if None:
+    if not isinstance(roman_string, str):
+        return 0
+    if roman_string is None:
         return 0
 
     val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
