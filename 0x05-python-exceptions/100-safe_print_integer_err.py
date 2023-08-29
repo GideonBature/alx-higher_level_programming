8329697ba9cc4e762550e782df0d94f027c70364
Gideon Bature <infoaboutgideon@gmail.com>
2023-08-29 01:58:34 +0100

    add edge case for TypeError

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -9,3 +9,6 @@ def safe_print_integer_err(value):
     except ValueError as err:
         print("Exception:", err, file=sys.stderr)
         return False
+    except TypeError as err:
+        print("Exception:", err, file=sys.stderr)
+        return False
