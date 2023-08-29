8f69895e52c2303b76324f032d345571c9745d27
Gideon Bature <infoaboutgideon@gmail.com>
2023-08-29 01:12:18 +0100

    add the continue statement

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -9,11 +9,14 @@ def list_division(my_list_1, my_list_2, list_length):
         except ZeroDivisionError:
             print("division by 0")
             result.append(0)
+            continue
         except TypeError:
             print("wrong type")
             result.append(0)
+            continue
         except IndexError:
             print("out of range")
             result.append(0)
+            continue
 
     return result
