f3e35afeb525a2ea0efb84afb4424ed250879586
Gideon Bature <infoaboutgideon@gmail.com>
2023-08-29 02:56:47 +0100

    removed individual exception kinds and used the general class Exception

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -6,15 +6,6 @@ def safe_function(fct, *args):
     try:
         fct(*args)
         return fct(*args)
-    except IndexError as err:
-        print("Exception:", err, file=sys.stderr)
-        return None
-    except ValueError as err:
-        print("Exception:", err, file=sys.stderr)
-        return None
-    except TypeError as err:
-        print("Exception:", err, file=sys.stderr)
-        return None
-    except ZeroDivisionError as err:
+    except Exception as err:
         print("Exception:", err, file=sys.stderr)
         return None
