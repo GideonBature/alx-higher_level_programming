c08298a60cf036cc71070d4064b95cce1c5b92b3
Gideon Bature <infoaboutgideon@gmail.com>
2023-08-29 01:14:45 +0100

    remove multiple continue statements

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -9,15 +9,12 @@ def list_division(my_list_1, my_list_2, list_length):
         except ZeroDivisionError:
             print("division by 0")
             result.append(0)
-            continue
         except TypeError:
             print("wrong type")
             result.append(0)
-            continue
         except IndexError:
             print("out of range")
             result.append(0)
-            continue
         finally:
             continue
 
