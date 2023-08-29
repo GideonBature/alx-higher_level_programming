e4d4dc59e2c2ddc9b6b8ec5a9535a0e8e34f45ec
Gideon Bature <infoaboutgideon@gmail.com>
2023-08-29 01:13:52 +0100

    add the finally statement

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -18,5 +18,7 @@ def list_division(my_list_1, my_list_2, list_length):
             print("out of range")
             result.append(0)
             continue
+        finally:
+            continue
 
     return result
