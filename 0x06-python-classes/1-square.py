2874af8073a7db21adb7f318acd3d80d732f0af9
Gideon Bature <infoaboutgideon@gmail.com>
2023-08-29 21:19:54 +0100

    a class Square that defines a square by: (based on 0-square.py)

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Defines a class square"""
+
+class Square:
+    """class defines square object"""
+    def __init__(self, size):
+        """__init__ method
+
+        Args:
+            size: size of square
+        """
+        self.__size = size
