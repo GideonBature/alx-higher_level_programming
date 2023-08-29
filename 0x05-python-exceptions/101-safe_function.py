1e9e2556c3d49972aaa49ffcb8d9bc2993ecffad
Gideon Bature <infoaboutgideon@gmail.com>
2023-08-29 03:01:48 +0100

    optimizes the code

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -4,8 +4,8 @@ import sys
 
 def safe_function(fct, *args):
     try:
-        fct(*args)
-        return fct(*args)
+        result = fct(*args)
+        return result
     except Exception as err:
         print("Exception:", err, file=sys.stderr)
         return None
