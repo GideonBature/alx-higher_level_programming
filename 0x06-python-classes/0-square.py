e4125c3785277783655be562afa6eca887958006
Gideon Bature <infoaboutgideon@gmail.com>
2023-08-29 21:02:21 +0100

    fix docstring

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,16 +1,5 @@
 #!/usr/bin/python3
 
 class Square:
-    """A class representing a square.
-
-    This class defines the basic structure of a square. It does not have any
-    methods or attributes yet, but it serves as a starting point for creating
-    and working with squares.
-
-    Attributes:
-        None
-
-    Methods:
-        None
-    """
+    """A class that defines a square."""
     pass
