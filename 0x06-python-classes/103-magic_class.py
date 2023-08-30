e5ceb16900ec8238d80a3d64b58e1c8f62ba2981
Gideon Bature <infoaboutgideon@gmail.com>
2023-08-30 04:21:33 +0100

    fix pycodestyle error

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,42 +1,42 @@
 #!/usr/bin/python3
 
-"""This Module defines a MagicClass that reps a circle with radius"""
+"""Module defines a MagicClass that reps a circle with radius"""
 
 import math
 
-"""kkdkdk"""
+"""import math"""
 
 
 class MagicClass:
     """
-    A class representing a circle and providing methods to calculate its area and circumference.
+    A class represents a circle, provides methods to cal area & circumference.
 
     Attributes:
         _MagicClass__radius (int or float): The radius of the circle.
 
     Methods:
-        __init__(self, _MagicClass__radius=0): Constructor to initialize the circle with a radius.
+        Constructor initialize circle w/radius.
         area(self): Calculate the area of the circle.
         circumference(self): Calculate the circumference of the circle.
     """
 
     def __init__(self, _MagicClass__radius=0):
-                """
-        Initialize the MagicClass with a given radius.
+        """Initialize the MagicClass with a given radius.
 
         Args:
-            _MagicClass__radius (int or float): The radius of the circle.
+            _MagicClass__radius (int or float): radius of circle.
 
         Raises:
             TypeError: If the provided radius is not a number.
         """
+
         if ((type(_MagicClass__radius) is not int) and
                 (type(_MagicClass__radius) is not float)):
             raise TypeError('radius must be a number')
         self._MagicClass__radius = _MagicClass__radius
 
     def area(self):
-         """
+        """
         Calculate the area of the circle.
 
         Returns:
