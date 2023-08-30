6f01dacc12d9122ef2c50a1379f4ee5051b6eb2e
Gideon Bature <infoaboutgideon@gmail.com>
2023-08-30 04:12:21 +0100

    add docstring to magic file

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,15 +1,54 @@
 #!/usr/bin/python3
+
+"""This Module defines a MagicClass that reps a circle with radius"""
+
 import math
+
+"""kkdkdk"""
 
 
 class MagicClass:
-    def __init__(self, radius):
-        if type(radius) is not int and type(radius) is not float:
+    """
+    A class representing a circle and providing methods to calculate its area and circumference.
+
+    Attributes:
+        _MagicClass__radius (int or float): The radius of the circle.
+
+    Methods:
+        __init__(self, _MagicClass__radius=0): Constructor to initialize the circle with a radius.
+        area(self): Calculate the area of the circle.
+        circumference(self): Calculate the circumference of the circle.
+    """
+
+    def __init__(self, _MagicClass__radius=0):
+                """
+        Initialize the MagicClass with a given radius.
+
+        Args:
+            _MagicClass__radius (int or float): The radius of the circle.
+
+        Raises:
+            TypeError: If the provided radius is not a number.
+        """
+        if ((type(_MagicClass__radius) is not int) and
+                (type(_MagicClass__radius) is not float)):
             raise TypeError('radius must be a number')
-        self.__radius = float(radius)
+        self._MagicClass__radius = _MagicClass__radius
 
     def area(self):
-        return self.__radius ** 2 * math.pi
+         """
+        Calculate the area of the circle.
+
+        Returns:
+            float: The area of the circle.
+        """
+        return (math.pi * self._MagicClass__radius ** 2)
 
     def circumference(self):
-        return 2 * math.pi * self.__radius
+        """
+        Calculate the circumference of the circle.
+
+        Returns:
+            float: The circumference of the circle.
+        """
+        return (2 * math.pi * self._MagicClass__radius)
