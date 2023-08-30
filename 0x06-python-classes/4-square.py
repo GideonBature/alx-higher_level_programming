016499036bf1eeeb8b77d82e327bfcfbe313e240
Gideon Bature <infoaboutgideon@gmail.com>
2023-08-30 01:56:01 +0100

    a class Square that defines a square by: (based on 3-square.py)

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Defines a class square"""
+
+
+class Square:
+
+    """class defines square object"""
+
+    def __init__(self, size=0):
+
+        """__init__ method
+
+        Args:
+            size: size of square
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """Getter Function"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """ public instance method that
+            returns area
+        """
+        product = (self.__size) * (self.__size)
+        return product
