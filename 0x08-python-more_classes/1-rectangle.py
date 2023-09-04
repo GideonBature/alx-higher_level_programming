27edb4612456b18532505105500c40fdb6a575a7
Gideon Bature <infoaboutgideon@gmail.com>
2023-09-04 21:06:07 +0100

    take height to the top and width to the bottom

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -12,27 +12,33 @@ class Rectangle:
         self.__height = height
 
     @property
-    def width(self):
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        elif value < 0:
-            raise ValueError("width must be >= 0")
-        else:
-            self.__width = value
-
-    @property
     def height(self):
+        """Getter for height attribute
+        """
         return self.__height
 
     @height.setter
     def height(self, value):
+        """Setter for height attribute.
+        """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
-        else:
-            self.__height = value
+        self.__height = value
+
+    @property
+    def width(self):
+        """Getter for width attribute
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter for width attribute
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
