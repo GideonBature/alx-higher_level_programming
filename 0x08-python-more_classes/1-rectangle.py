e2f6160bf02fb51f2884334ffd9903db034de04d
Gideon Bature <infoaboutgideon@gmail.com>
2023-09-05 00:01:38 +0100

    add return statement to setter attributes

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -26,6 +26,7 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
+        return (self.__width)
 
     @property
     def height(self):
@@ -42,3 +43,4 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+        return (self.__height)
