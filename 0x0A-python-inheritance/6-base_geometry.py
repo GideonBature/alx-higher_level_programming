def3f3921b91a411a2954e10f55f457ef1f3b60a
Gideon Bature <infoaboutgideon@gmail.com>
2023-09-11 22:02:34 +0100

    a class BaseGeometry (based on 5-base_geometry.py).

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""a class with instance method
+"""
+
+
+class BaseGeometry:
+    """a base class
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
