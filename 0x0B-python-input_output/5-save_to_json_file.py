abe1bcb0f6a26b114e64758187732c3666dc1cdc
Gideon Bature <infoaboutgideon@gmail.com>
2023-09-12 23:32:29 +0100

    fix the documentation

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -8,7 +8,8 @@ def save_to_json_file(my_obj, filename):
     """Writes an object to a JSON representation
 
     Args:
-        my_str: the string parameter
+        my_obj: the object parameter
+        filename: (str): name of file
 
     """
     with open(filename, 'w', encoding='UTF8') as json_file:
