186719924899e28380569846d83df5b9ab97a2cf
Gideon Bature <infoaboutgideon@gmail.com>
2023-09-13 04:09:29 +0100

    a class Student that defines a student by: (based on 10-student.py)

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""Student to JSON with filter
+"""
+
+
+class Student:
+    """Student class
+    """
+    def __init__(self, first_name, last_name, age):
+        """Instantiates public attributes
+
+        Args:
+            first_name (str): first name parameter
+            last_name (str): last name parameter
+            age: (int) age parameter
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of
+        a Student instance.
+
+        Returns:
+        dict: A dictionary with the student's information
+        """
+        if attrs is None:
+            return {"first_name": self.first_name,
+                    "last_name": self.last_name,
+                    "age": self.age
+                    }
+        else:
+            s = {attr: getattr(self, attr) for attr
+                 in attrs if hasattr(self, attr)}
+            return s
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of the Student
+        instance
+
+        Args:
+            json (dict): a dictionary
+        """
+        for key, value in json.items():
+            setattr(self, key, value)
