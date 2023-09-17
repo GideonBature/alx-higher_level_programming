f46aaf75abe4aa51c532e42abf6172a17f1ffe02
Gideon Bature <infoaboutgideon@gmail.com>
2023-09-17 07:18:12 +0100

    fix pycodestyle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -8,8 +8,9 @@ class Base:
     project
     """
     __nb_objects = 0
+
     def __init__(self, id=None):
-        if id != None:
+        if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
