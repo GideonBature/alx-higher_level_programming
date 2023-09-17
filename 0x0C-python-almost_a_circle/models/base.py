fdd43da5caeb09183d0f0c45907acd3fb372305b
Gideon Bature <infoaboutgideon@gmail.com>
2023-09-17 07:15:53 +0100

    create directories and files

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Base Class
+"""
+
+
+class Base:
+    """ The Base of all other classes in this
+    project
+    """
+    __nb_objects = 0
+    def __init__(self, id=None):
+        if id != None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
