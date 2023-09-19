cfcf466e4c0898f7b8843bb98a2da92ce74565cf
Gideon Bature <infoaboutgideon@gmail.com>
2023-09-19 09:34:40 +0100

    using super to inherit all atrributes

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -17,9 +17,6 @@ class Square(Rectangle):
             id (int): The identity of the new Square.
         """
         super().__init__(size, size, x, y, id)
-        self.__size = size
-        self.__x = x
-        self.__y = y
 
     @property
     """Get/set the size of the Square.
