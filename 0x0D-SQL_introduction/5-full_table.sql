4d62473f22e093e6d8355f5097f2ea515fa3829d
Gideon Bature <infoaboutgideon@gmail.com>
2023-10-17 22:24:27 +0100

    a script that prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server.

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,8 @@
 -- prints the full description of the table - first_table
 -- from the database gbtn_0c_0 in your MySQL server
-SHOW COLUMNS FROM first_table;
+-- Replace 'hbtn_0c_0' and 'first_table' with your database and table names
+USE hbtn_0c_0;
+
+SELECT COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH, IS_NULLABLE, COLUMN_DEFAULT
+FROM information_schema.COLUMNS
+WHERE TABLE_NAME = 'first_table';
