d021fbb7cf0fb00000166a8b6295c12d28eb7096
Gideon Bature <infoaboutgideon@gmail.com>
2023-10-18 01:33:24 +0100

    fix code

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,6 +1,4 @@
 -- prints the full description of the table - first_table
 -- from the database gbtn_0c_0 in your MySQL server
 -- Replace 'hbtn_0c_0' and 'first_table' with your database and table names
-USE hbtn_0c_0;
-
-SHOW CREATE TABLE `first_table`;
+SHOW CREATE TABLE;
