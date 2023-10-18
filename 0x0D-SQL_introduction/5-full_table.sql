b1dd21e2e0e0077c2572f95bcb7b0e3d721c9cbc
Gideon Bature <infoaboutgideon@gmail.com>
2023-10-18 01:31:03 +0100

    fix code

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -3,6 +3,4 @@
 -- Replace 'hbtn_0c_0' and 'first_table' with your database and table names
 USE hbtn_0c_0;
 
-SELECT COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH, IS_NULLABLE, COLUMN_DEFAULT
-FROM information_schema.COLUMNS
-WHERE TABLE_NAME = 'first_table';
+SHOW CREATE TABLE;
