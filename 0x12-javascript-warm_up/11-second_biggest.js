f8f91f33166eb1b10ccbf78adb17ecd68c24d88e
GideonBature <infoaboutgideon@gmail.com>
2023-11-14 03:58:40 +0100

    fix code.

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,8 +3,9 @@
 const args = process.argv;
 const len = args.length;
 if (len <= 3) {
-  console.log(0);
+	console.log(0);
 } else {
-  const sortedArgs = args.sort();
-  console.log(sortedArgs[len - 2]);
+	const intArray = args.map(str => parseInt(str));
+	const sortedIntArray = intArray.sort((a, b) => b - a);
+	console.log(sortedIntArray[3]);
 }
