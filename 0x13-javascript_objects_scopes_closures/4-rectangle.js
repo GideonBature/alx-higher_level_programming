7cb97ce2064fb9e047ff4f5d66d3ed6c3cd4ee65
GideonBature <infoaboutgideon@gmail.com>
2023-11-14 23:47:14 +0100

    a class Rectangle that defines a rectangle

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      const str = 'X';
+      const repeatedStr = str.repeat(this.width);
+      console.log(repeatedStr);
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
