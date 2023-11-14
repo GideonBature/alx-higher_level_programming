a08939036ab6441bffb224a6fbaf02c1b5414213
GideonBature <infoaboutgideon@gmail.com>
2023-11-14 03:59:24 +0100

    fix semistandard style

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,9 +3,9 @@
 const args = process.argv;
 const len = args.length;
 if (len <= 3) {
-	console.log(0);
+  console.log(0);
 } else {
-	const intArray = args.map(str => parseInt(str));
-	const sortedIntArray = intArray.sort((a, b) => b - a);
-	console.log(sortedIntArray[3]);
+  const intArray = args.map(str => parseInt(str));
+  const sortedIntArray = intArray.sort((a, b) => b - a);
+  console.log(sortedIntArray[3]);
 }
