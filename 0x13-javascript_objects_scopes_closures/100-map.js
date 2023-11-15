e107223cbd3e84975982f92b36ed1d0105abb8aa
GideonBature <infoaboutgideon@gmail.com>
2023-11-15 10:57:30 +0100

    include index for mapping

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -2,7 +2,7 @@
 
 const list = require('./100-data.js').list;
 
-const newList = list.map((x) => x * list.indexOf(x));
+const newList = list.map((value, index) => value * index);
 
 console.log(list);
 console.log(newList);
