12fce5cb06d10d30c300af1f10cd2641906c0d60
GideonBature <infoaboutgideon@gmail.com>
2023-11-18 06:55:44 +0100

    fix code to use format instead of format specifier

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     p = 3306
     db = MySQLdb.connect(host=lh, port=p, user=usr, passwd=pwd, db=datab)
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name = %s", (_name,))
+    cursor.execute("SELECT * FROM states WHERE name = '{}'".format(_name))
     states = cursor.fetchall()
 
     for state in states:
