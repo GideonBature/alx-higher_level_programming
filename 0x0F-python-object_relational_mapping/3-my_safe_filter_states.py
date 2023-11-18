b1a0cd49dc762816b5274b547e4673613267c3b8
GideonBature <infoaboutgideon@gmail.com>
2023-11-18 05:34:43 +0100

    a script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument. - safe from MySQL Injection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""a script that lists all states from the database hbtn_0e_0_usa:
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+
+    usr, pwd, datab, _name = sys.argv[1:5]
+    lh = "localhost"
+    p = 3306
+    db = MySQLdb.connect(host=lh, port=p, user=usr, passwd=pwd, db=datab)
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name = %s", (_name,))
+    states = cursor.fetchall()
+
+    for state in states:
+        print(state)
+
+    cursor.close()
+    db.close()
