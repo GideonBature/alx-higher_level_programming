0a7aae79ce8b86885273f607b94d944854279d3a
GideonBature <infoaboutgideon@gmail.com>
2023-12-01 21:22:53 +0100

    a Python script that fetches https://alx-intranet.hbtn.io/status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""fetches https://alx-intranet.hbtn.io/status
+"""
+from urllib.request import urlopen
+
+if __name__ == '__main__':
+    url = "https://alx-intranet.hbtn.io/status"
+    with urlopen(url) as response:
+        body = response.read()
+
+        print("Body Response:")
+        print(f"\t- type: {type(body)}")
+        print(f"\t- content: {body}")
+        print(f"\t- utf8 content: {body.decode('utf-8')}")
