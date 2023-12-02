f1fee82bf8e6bbf6589c3ed2c5f6b69da00d8f09
GideonBature <infoaboutgideon@gmail.com>
2023-12-02 02:26:43 +0100

    fix code #4

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -7,6 +7,7 @@ import sys
 import urllib.parse
 import urllib.request
 
+
 if __name__ == '__main__':
     url = sys.argv[1]
 
---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""takes in url, sends a request to the url
+usage: ./3-error_code.py <url>
+return: body of response(utf-8)
+"""
+
