ad78d5c1e5834e7c3a81789af20889aa028451cb
GideonBature <infoaboutgideon@gmail.com>
2023-12-02 01:53:49 +0100

    a Python script that takes in a URL and an email, sends a POST request to the passed URL with the email as a parameter, and displays the body of the response (decoded in utf-8)

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""a Python script that takes in a URL and an email,
+sends a POST request to the passed URL with the email as a parameter,
+and displays the body of the response (decoded in utf-8)
+"""
+from urllib.request import urlopen, Request
+from urllib.parse import urlencode
+import sys
+
+url = sys.argv[1]
+email = sys.argv[2]
+
+data = {"email": email}
+
+encoded_data = urlencode(data).encode('ascii')
+
+request = Request(url, data=encoded_data, method='POST')
+
+request.add_header('Content-Type', 'application/x-www-form-urlencoded')
+
+with urlopen(request) as response:
+    response_data = response.read()
+    decode_response_data = response_data.decode('utf-8')
+
+    print(f"{decode_response_data}")
