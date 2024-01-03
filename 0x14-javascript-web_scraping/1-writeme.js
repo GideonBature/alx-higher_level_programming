845294726b711a8d72ee0926e846aa4e839fa668
GideonBature <infoaboutgideon@gmail.com>
2024-01-03 15:04:53 +0100

    a script that writes a string to a file.

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// a script that writes a string to a file.
+const process = require('process');
+const fs = require('fs');
+
+const filePath = process.argv[2];
+const string = process.argv[3];
+
+fs.writeFile(filePath, string, 'utf8', (err) => {
+  if (err) {
+    console.error(err);
+  }
+});
