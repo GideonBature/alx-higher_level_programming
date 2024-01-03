b235190ca4539c45f3dc62e7a58f31f55770b904
GideonBature <infoaboutgideon@gmail.com>
2024-01-03 14:55:02 +0100

    a script that reads and prints the content of a file.

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+// script that reads and prints
+// the content of a file
+const process = require('process');
+const fs = require('fs');
+
+const filePath = process.argv[2];
+
+fs.readFile(filePath, 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data);
+  }
+});
