1c790db9c7a48d9638840641170a6de9c94d99f4
GideonBature <infoaboutgideon@gmail.com>
2024-01-03 23:35:50 +0100

    a script that gets the contents of a webpage and stores it in a file.

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+// a script that gets the contents of
+// a webpage and stores it in a file.
+const process = require('process');
+const request = require('request');
+const fs = require('fs');
+
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+request ( url, (error, response, body) => {
+	if (error) {
+		console.error(error);
+	} else {
+		fs.writeFile ( filePath, body, 'utf8', (err) => {
+			if (err) {
+				console.error(err);
+			}
+		});
+	}
+});
