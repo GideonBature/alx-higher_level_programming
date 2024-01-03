5d7a1513cb2a28b072af67b26f15be2fbaf9173f
GideonBature <infoaboutgideon@gmail.com>
2024-01-03 15:24:03 +0100

    a script that display the status code of a GET request.

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// a script that display the status code of a `GET` request.
+const process = require('process');
+const request = require('request');
+
+const url = process.argv[2];
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log('code: ' + response.statusCode);
+  }
+});
