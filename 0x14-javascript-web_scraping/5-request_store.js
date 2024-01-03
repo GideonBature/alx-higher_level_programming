d5b910cf46f0b3c5a3b4d7811b7c856001ba118f
GideonBature <infoaboutgideon@gmail.com>
2024-01-03 23:37:17 +0100

    fix semistandard

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -8,14 +8,14 @@ const fs = require('fs');
 const url = process.argv[2];
 const filePath = process.argv[3];
 
-request ( url, (error, response, body) => {
-	if (error) {
-		console.error(error);
-	} else {
-		fs.writeFile ( filePath, body, 'utf8', (err) => {
-			if (err) {
-				console.error(err);
-			}
-		});
-	}
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    fs.writeFile(filePath, body, 'utf8', (err) => {
+      if (err) {
+        console.error(err);
+      }
+    });
+  }
 });
