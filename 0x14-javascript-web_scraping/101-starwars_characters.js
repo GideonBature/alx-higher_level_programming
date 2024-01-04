c667f5cfeaa40d31a35daf8d5dcf53a47412a756
GideonBature <infoaboutgideon@gmail.com>
2024-01-04 01:33:21 +0100

    a script that prints all characters of a Star Wars movie: - orderly

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+// a script that prints all characters of a Star Wars movie
+const process = require('process');
+const request = require('request');
+
+const movieId = process.argv[2];
+
+const url = 'https://swapi-api.alx-tools.com/api/films/' + movieId + '/';
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    const data = JSON.parse(body);
+    const chars = data.characters;
+
+    fetchCharsRecursively(chars, 0);
+  }
+});
+
+function fetchCharsRecursively (urls, index) {
+  if (index < urls.length) {
+    request(urls[index], (error, response, body) => {
+      if (error) {
+        console.error(error);
+      } else {
+        const charData = JSON.parse(body);
+        console.log(charData.name);
+        fetchCharsRecursively(urls, index + 1);
+      }
+    });
+  }
+}
